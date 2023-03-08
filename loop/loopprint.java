a3e13b338414814401a5a550021e14f235eb3f93
KACHHAP LILY <lilykumarikachhap@gmail.com>
2023-03-08 21:08:20 +0530

    loop for,while,do-while

---
+++ loop/loopprint.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class loopprint {
+   
+    public static void main(String[] args) {
+        for(int i =0;i<=20;i++){
+            System.out.println(i);
+        }
+        int j=0;
+        do{
+            System.out.println(j);
+            j++;
+        }
+        while(j<22);
+
+        
+    }
+}
